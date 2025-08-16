003b20f94f0b21d7cf6d11e217e7818e4a7f7f6a
josedavidlunamolano24-afk <josedavidlunamolano24@gmail.com>
2025-08-16 10:29:17 -0500

    este es mi primer commit

---
+++ comparacion.js
@@ -0,0 +1,21 @@
+let edad = parseInt(prompt("ingresa tu edad"));
+if (edad > 0 && edad < 12) {
+    alert("infantil");
+}
+else if (edad >= 13 && edad <= 17) {
+    alert("adolescente");
+}
+else if (edad >= 18 && edad <= 59) {
+    alert("adulto");
+}
+else if (edad >= 60) {
+    alert("adulto mayor");
+}
+
+//operador ternario
+
+(edad > 0 && edad < 12)? alert("infantil") :
+(edad >= 13 && edad <= 17)? alert("adolescente") :
+(edad >= 18 && edad <= 59)? alert("adulto") :
+(edad >= 60)? alert("adulto mayor") :
+alert("edad no válida");
